63a95920600c9dd9702ed6874ced171a38dc602b
Aparup Banerjee <aparup_banerjee@dialog.com.au>
2018-11-01 09:44:14 +0800

    prep for voc21 and rummyoo data

--- voc_annotation.py
+++ voc_annotation.py
@@ -3,11 +3,12 @@ from os import getcwd
 
 sets=[('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
 
-classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor", "rummy o"]
 
 
 def convert_annotation(year, image_id, list_file):
-    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
+    ## in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
+    in_file = open('../rummy-data-backup/convert_YOLO_to_VOC/converted_labels/%s.xml'%(image_id))
     tree=ET.parse(in_file)
     root = tree.getroot()
 
@@ -17,6 +18,7 @@ def convert_annotation(year, image_id, list_file):
         if cls not in classes or int(difficult)==1:
             continue
         cls_id = classes.index(cls)
+        # cls_id = 21
         xmlbox = obj.find('bndbox')
         b = (int(xmlbox.find('xmin').text), int(xmlbox.find('ymin').text), int(xmlbox.find('xmax').text), int(xmlbox.find('ymax').text))
         list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
@@ -24,10 +26,12 @@ def convert_annotation(year, image_id, list_file):
 wd = getcwd()
 
 for year, image_set in sets:
-    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
-    list_file = open('%s_%s.txt'%(year, image_set), 'w')
+    # image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
+    image_ids = open('../rummy-data-backup/convert_YOLO_to_VOC/%s.txt'%(image_set)).read().strip().split()
+    # list_file = open('%s_%s.txt'%(year, image_set), 'w')
+    list_file = open('%s.txt'%(image_set), 'w')
     for image_id in image_ids:
-        list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg'%(wd, year, image_id))
+        list_file.write('../rummy-data-backup/convert_YOLO_to_VOC/images/%s.jpg'%(image_id))
         convert_annotation(year, image_id, list_file)
         list_file.write('\n')
     list_file.close()
